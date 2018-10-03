99d383e706b1269fbd31c34161bfc99e73887e9d
Lurid <epticid@gmail.com>
2018-10-03 23:33:04 +0600

    11d

--- 2D_Proj3/2D_Proj/Assets/Scripts/UpgradeTree.cs
+++ 2D_Proj3/2D_Proj/Assets/Scripts/UpgradeTree.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class perkUpgrade
 {
     byte NeededLevel;
+    Action action;
+
+    public perkUpgrade (byte _NeededLevel, Action _action)
+    {
+        NeededLevel = _NeededLevel;
+        action = _action;
+    }
 }
 
 public class pperk
@@ -14,13 +22,15 @@ public class pperk
     string name;
     string description;
     uint[] nextids;
+    public perkUpgrade[] perkUpgradeLevels;
 
-    public pperk(uint _id, string _name, string _description, uint[] _nextids)
+    public pperk(uint _id, string _name, string _description, uint[] _nextids, perkUpgrade[] _perkUpgradeLevels)
     {
         id = _id;
         name = _name;
         description = _description;
         nextids = _nextids;
+        perkUpgradeLevels = _perkUpgradeLevels;
     }
 }
 
@@ -43,15 +53,17 @@ public class UpgradeTree : MonoBehaviour {
     public Text Upper;
     public Text Bottom;
     public skillType SkillType;
+    public Player player;
 
     public skilltree Illusion = new skilltree("Иллюзия", (skillType)0, new pperk[] {
-        new pperk(0x000F2CA9, "Новичок школы иллюзии", "Заклинания школы иллюзии уровня новичка расходуют вдвое меньше магии.", new uint[] { 0x000153D0, 0x000C44C3, 0x000581E2 })
+        new pperk(0x000F2CA9, "Новичок школы иллюзии", "Заклинания школы иллюзии уровня новичка расходуют вдвое меньше магии.", new uint[] { 0x000153D0, 0x000C44C3, 0x000581E2 }, new perkUpgrade[] { new perkUpgrade(0, delegate () { /**/ })}),
+        new pperk(0x000F2CA9, "Двойная иллюзия", "При сотворении заклинания школы иллюзий с двух рук получается его более сильный вариант.", new uint[] { 0x000153D0, 0x000C44C3, 0x000581E2 }, new perkUpgrade[] { new perkUpgrade(0, delegate () { /**/ })})
     });
 
     void Start()
     {
         UpgradeSkillPointsText();
-
+        //Action myAction = delegate () { int i = 1; };// void { int i = 1; }; //new Action<>(
     }
 
     public void UpgradeSkillPointsText()
