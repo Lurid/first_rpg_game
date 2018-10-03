0b0b7a02658fa029c422fb7abe6202e4d5e37356
Alex <epticid@gmail.com>
2018-10-03 18:53:55 +0600

    10w

--- 2D_Proj3/2D_Proj/Assets/Scripts/Player.cs
+++ 2D_Proj3/2D_Proj/Assets/Scripts/Player.cs
@@ -20,13 +20,28 @@ public class Player : MonoBehaviour {
     public Text PerkPointsText;
 
     void Start () {
-        FreePerkPoints = 10;
+        FreePerkPoints = 20;
         UpgradePerkPointsText();
     }
 	
 	public void TryLernPerk (Perk p) {
-        Debug.Log(p.Name + " clicked");
-        if ((p.CanBeLearned) && (FreePerkPoints > 0) && (Illusion > p.NeededLevel) && (!p.learned))
+        //Debug.Log(p.Name + " clicked");
+        if (!p.CanBeLearned) //&& (FreePerkPoints > 0) && (Illusion > p.NeededLevel) && (!p.learned)
+        {
+            Debug.Log("Необходимо выучить предыдущие навыки.");
+        }
+        else if (Illusion < p.NeededLevel)
+        {
+            Debug.Log("У вас недостаточный уровень навыка " + p.tree.SkillName + ".");
+        }
+        else if (FreePerkPoints < 1)
+        {
+            Debug.Log("Недостаточно очков навыков.");
+        }
+        else if (p.learned)
+        {
+            Debug.Log("Навык уже выучен.");
+        } else 
         {
             FreePerkPoints--;
             UpgradePerkPointsText();
@@ -37,5 +52,32 @@ public class Player : MonoBehaviour {
     public void UpgradePerkPointsText ()
     {
         PerkPointsText.text = "Свободных очков способностей: " + FreePerkPoints;
+    }
+
+    public byte GetSkillValue (skillType sT)
+    {
+        byte result = 0;
+        switch (sT)
+        {
+            case skillType.Illusion: result = Illusion; break;
+            case skillType.Conjuration: result = Conjuration; break;
+            case skillType.Destruction: result = Destruction; break;
+            case skillType.Restoration: result = Restoration; break;
+            case skillType.Alteration: result = Alteration; break;
+            case skillType.Enchanting: result = Enchanting; break;
+            case skillType.Smithing: result = Smithing; break;
+            case skillType.HeavyArmor: result = HeavyArmor; break;
+            case skillType.Block: result = Block; break;
+            case skillType.Twohanded: result = Twohanded; break;
+            case skillType.Onehanded: result = Onehanded; break;
+            case skillType.Marksman: result = Marksman; break;
+            case skillType.LightArmor: result = LightArmor; break;
+            case skillType.Sneak: result = Sneak; break;
+            case skillType.Lockpicking: result = Lockpicking; break;
+            case skillType.Pickpocket: result = Pickpocket; break;
+            case skillType.Speechcraft: result = Speechcraft; break;
+            case skillType.Alchemy: result = Alchemy; break;
+        }
+    return result;
     }
 }
--- 2D_Proj3/2D_Proj/Assets/Scripts/UpgradeTree.cs
+++ 2D_Proj3/2D_Proj/Assets/Scripts/UpgradeTree.cs
@@ -3,14 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class perkUpgrade
+{
+    byte NeededLevel;
+}
+
+public class pperk
+{
+    uint id;
+    string name;
+    string description;
+    uint[] nextids;
+
+    public pperk(uint _id, string _name, string _description, uint[] _nextids)
+    {
+        id = _id;
+        name = _name;
+        description = _description;
+        nextids = _nextids;
+    }
+}
+
+public class skilltree
+{
+    public string name;
+    public skillType SkillType;
+    public pperk[] perks;
+
+    public skilltree(string _name, skillType _SkillType, pperk[] _perks)
+    {
+        name = _name;
+        SkillType = _SkillType;
+        perks = _perks;
+    }
+}
+
 public class UpgradeTree : MonoBehaviour {
     public string SkillName;
-    public skillType SkillType;
     public Text Upper;
     public Text Bottom;
+    public skillType SkillType;
+
+    public skilltree Illusion = new skilltree("Иллюзия", (skillType)0, new pperk[] {
+        new pperk(0x000F2CA9, "Новичок школы иллюзии", "Заклинания школы иллюзии уровня новичка расходуют вдвое меньше магии.", new uint[] { 0x000153D0, 0x000C44C3, 0x000581E2 })
+    });
+
+    void Start()
+    {
+        UpgradeSkillPointsText();
+
+    }
 
     public void UpgradeSkillPointsText()
     {
-        Upper.text = "Уровень навыка "+ SkillName + ": " + transform.GetComponentInParent<Player> ();
+        Upper.text = "Уровень навыка "+ SkillName + ": " + transform.GetComponentInParent<Player> ().GetSkillValue(SkillType);
     }
 }
