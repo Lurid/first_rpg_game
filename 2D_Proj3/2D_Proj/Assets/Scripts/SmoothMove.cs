0678316f3ed01f8e588a27def591868bd73a8bbb
Lurid <epticid@gmail.com>
2018-10-10 00:10:10 +0600

    22d

--- 2D_Proj3/2D_Proj/Assets/Scripts/SmoothMove.cs
+++ 2D_Proj3/2D_Proj/Assets/Scripts/SmoothMove.cs
@@ -5,17 +5,19 @@ using UnityEngine.UI;
 using UnityEditor;
 
 public class SmoothMove : MonoBehaviour {
-    public AnimationCurve positionCurve;
-    public AnimationCurve sizeCurve;
+    public AnimationCurve Curve;
 
     public Vector2 startPos;
     public Vector2 endPos;
     private Vector2 differencePos;
-    public Vector2 sizeModificator = Vector2.one;
+    public Vector2 startSize;
+    public Vector2 modifierSize;
     public float LastActionTime = 1000f;
     public bool UoD = true;
     private bool Show = false;
     private bool UoDvector = true;
+    public Transform BackgroundObj;
+    public float modifierSize2 = 3f;
 
     public float MoveTimeInSeconds = 4f;
 
@@ -30,7 +32,7 @@ public class SmoothMove : MonoBehaviour {
         if (Show)
         {
             float f = (Time.time - LastActionTime) / MoveTimeInSeconds;
-            Set(((UoDvector) ? 1 : 0) - f * ((UoDvector) ? -1 : 1));
+            Set((((UoDvector) ? 1 : 0) - f) * ((UoDvector) ? 1 : -1)); //* ((UoDvector) ? -1 : 1)
         }
 
         if ((Show == true) && (Time.time > (LastActionTime + MoveTimeInSeconds)))
@@ -39,10 +41,17 @@ public class SmoothMove : MonoBehaviour {
             {
                 Set(1);
                 UoD = false;
+                transform.GetComponent<CanvasGroup>().alpha = 1;
+                transform.GetComponent<CanvasGroup>().blocksRaycasts = true;
+                transform.GetComponent<RectTransform>().localScale = new Vector3(startSize.x + modifierSize.x, startSize.y + modifierSize.y, 1);
             } else
             {
                 Set(0);
                 UoD = true;
+                transform.GetComponent<CanvasGroup>().alpha = 0;
+                transform.GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+                transform.GetComponent<RectTransform>().localScale = new Vector3(startSize.x, startSize.y, 1);
             }
             Show = false;
             UoDvector = !UoDvector;
@@ -51,8 +60,13 @@ public class SmoothMove : MonoBehaviour {
 
     void Set(float f)
     {
-        transform.GetComponent<RectTransform>().anchoredPosition = startPos + (positionCurve.Evaluate(f) * differencePos);
-        transform.GetComponent<RectTransform>().localScale = new Vector3((sizeCurve.Evaluate(f) * sizeModificator.x), (sizeCurve.Evaluate(f) * sizeModificator.y), 1);
+        Debug.Log("f = " + f);
+        transform.GetComponent<RectTransform>().anchoredPosition = startPos + (Curve.Evaluate(f) * differencePos);
+        transform.GetComponent<RectTransform>().localScale = new Vector3(startSize.x + (Curve.Evaluate(f) * modifierSize.x), startSize.y + (Curve.Evaluate(f) * modifierSize.y), 1);
+        transform.GetComponent<CanvasGroup>().alpha = f;
+        BackgroundObj.GetComponent<RectTransform>().anchoredPosition = startPos + (Curve.Evaluate(f) * differencePos);
+        BackgroundObj.GetComponent<RectTransform>().localScale = new Vector3((startSize.x + (Curve.Evaluate(f) * modifierSize.x)) * modifierSize2, (startSize.y + (Curve.Evaluate(f) * modifierSize.y)) * modifierSize2, 1);
+        BackgroundObj.GetComponent<CanvasGroup>().alpha = 1-f;
     }
 
     public void ShowIt()
